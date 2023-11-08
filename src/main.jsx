c024e24b0efab1b5e69f2a620050a95c4bf2f54e
UmiKami <ggoku02@gmail.com>
2023-11-07 22:06:01 -0500

    removed dev mode tag

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import Layout from './Layout.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <Layout />
-  </React.StrictMode>,
+
 )
